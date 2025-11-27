ceac2144683019970a1f5b86b165a659f9ae981b
PS49128_Dang Thanh Dat <thanhdatprok7@gmail.com>
2025-11-27 13:05:29 +0700

    nop bai lab6

--- Lab6/lab6.c
+++ Lab6/lab6.c
@@ -1,52 +1,49 @@
+//BÀI 1: TÍNH TRUNG BÌNH TỔNG CÁC SỐ CHIA HẾT CHO 3 TRONG MẢNG
 #include <stdio.h>
 #include <math.h>
-
 int main(){
     int n;
-    printf("Nhap do dai mang A: ");
+    printf("Nhap do dai mang A:");
     scanf("%d",&n);
-    int a[n]; 
+    int a[n];
     for (int i=0;i<n;i++){
-        printf("Nhap phan tu a[%d]: ",i);
+        printf("Nhap mang a[%d]:",i);
         scanf("%d",&a[i]);
     }
-    printf("\n--- Danh sach Mang A ---\n");
     for (int i=0;i<n;i++){
-        printf("A[%d]: %d\n", i, a[i]); 
+        printf("\nMang A:%d",a[i]);
     }
-    int S=0, dem=0;
+    int S=0,dem=0;
     for (int i=0;i<n;i++){
         if(a[i]%3==0){
-            S += a[i];
+            S +=a[i];
             dem++;
         }
     }
     if(dem>0){
         float TB = (float)S/dem;
-        printf("\nTrung binh cac so chia het cho 3: %.2f\n",TB);
+        printf("\nTrung binh cac so chia het cho 3: %.2f",TB);
     }else{
-        printf("\nKhong co so nao chia het cho 3 trong mang A!\n");
-    }ww
+        printf("\nKhong co so nao chia het cho 3!");
+    }
+//BÀI 2: TÌM GIÁ TRỊ LỚN NHẤT VÀ NHỎ NHẤT TRONG MẢNG
     int m;
-    printf("\n--- PHAN 2: Tim Max/Min ---\n");
-    printf("Nhap do dai mang B: ");
+    printf("\nNhap do dai mang B: ");
     scanf("%d",&m);
     int b[m];
     for(int j=0;j<m;j++){
-        printf("Nhap phan tu b[%d]: ",j);
-        scanf("%d",&b[j]); 
-    } 
-    printf("\n--- Danh sach Mang B ---\n");
-    for(int j=0;j<m;j++){
-        printf("B[%d]: %d\n", j, b[j]); 
+        printf("Nhap mang: b[%d]",j);
+        scanf("%d\n",&b[j]);
     }
-    int max = b[0];
-    int min = b[0]; 
+    for(int j=0;j<m;j++){
+        printf("Mang B:%d\n",j);
+    }
+    double max=b[0],min=b[0];
     for (int j = 1; j < m; j++){
         if (b[j] > max) max = b[j];
         if (b[j] < min) min = b[j];
     }
-    printf("\nGia tri lon nhat: %d\n", max);
-    printf("Gia tri nho nhat: %d\n", min);
+    printf("\nGia tri lon nhat: %.2lf", max);
+    printf("\nGia tri nho nhat: %.2lf\n", min);
     return 0;
 }
