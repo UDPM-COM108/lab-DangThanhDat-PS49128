fb761c4b799f4e4026af1ec6d587919a9d8b3006
PS49128_Dang Thanh Dat <thanhdatprok7@gmail.com>
2025-12-09 00:27:12 +0700

    4 bài chức năng

--- ASM/asm2.c
+++ ASM/asm2.c
@@ -24,16 +24,79 @@ int main(){
         scanf("%d", &luachon);
         switch (luachon){
             case 1:{
-                printf("\nnhap vao so nguyen: ");
-                
-            }break;
-            case 2:{
-               printf("\nnhap vao 2 so nguyen: ");
+                int x;
+                printf("\nNhap vao so nguyen X: ");
+                scanf("%d", &x);
+                printf("\n=> %d la so nguyen.", x);
+                int isPrime = 1;
+                if (x < 2) isPrime = 0;
+                else {
+                    for (int i = 2; i <= sqrt(x); i++) {
+                        if (x % i == 0) {
+                            isPrime = 0;
+                            break;
+                        }
+                    }
+                }
+                if (isPrime)
+                    printf("\n=> %d la so nguyen to.", x);
+                else
+                    printf("\n=> %d KHONG la so nguyen to.", x);
+                int r = sqrt(x);
+                if (r * r == x)
+                    printf("\n=> %d la so chinh phuong.", x);
+                else
+                    printf("\n=> %d KHONG la so chinh phuong.", x);
+                    printf("\n");
                 }break;
-            case 3:{
-                printf("\nnhap vao so gio hat: ");
-                }break;
-
+           case 2: {
+                int x, y;
+                printf("\nNhap vao so nguyen X: ");
+                scanf("%d", &x);
+                printf("Nhap vao so nguyen Y: ");
+                scanf("%d", &y);
+                int a = abs(x), b = abs(y);
+                if (a == 0 && b == 0) {
+                    printf("\n=> Khong the tinh UCLN/BCNN vi ca hai so deu bang 0.\n");
+                    break;
+                }
+                int aa = a, bb = b;
+                while (bb != 0) {
+                    int t = aa % bb;
+                    aa = bb;
+                    bb = t;
+                }
+                int ucln = aa;
+                long long bcnn = 0;
+                if (ucln != 0)
+                    bcnn = (long long)(a / ucln) * b;
+                printf("\n=> Uoc chung lon nhat (UCLN) cua %d va %d la: %d", x, y, ucln);
+                printf("\n=> Boi chung nho nhat (BCNN) cua %d va %d la: %lld\n", x, y, bcnn);
+            } break;
+             case 3: {
+                int start, end;
+                printf("\nNhap gio bat dau (12 - 23): ");
+                scanf("%d", &start);
+                printf("Nhap gio ket thuc (12 - 23): ");
+                scanf("%d", &end);
+                if (start < 12 || start > 23 || end < 12 || end > 23 || start >= end) {
+                    printf("\n>>> Gio khong hop le!");
+                    printf("\nQuán mo cua tu 12h den 23h va gio ket thuc phai lon hon gio bat dau.\n");
+                    break;
+                }
+                int gioHat = end - start;
+                float tongTien = 0;
+                if (gioHat <= 3) {
+                    tongTien = gioHat * 150000;
+                } else {
+                    tongTien = 3 * 150000 + (gioHat - 3) * (150000 * 0.7);
+                }
+                if (start >= 14 && start <= 17) {
+                    tongTien *= 0.9;
+                }
+                printf("\nSo gio hat: %d", gioHat);
+                printf("\n=> Tong tien can thanh toan: %.0f VND\n", tongTien);
+            } break;
             case 4:{
                 int sodien;
                 double tiendien = 0;
@@ -47,7 +110,7 @@ int main(){
                 if(sodien > 200) { tiendien += (sodien-200)*2536; sodien=200; }
                 if(sodien > 100) { tiendien += (sodien-100)*2014; sodien=100; }
                 if(sodien > 50)  { tiendien += (sodien-50)*1734; sodien=50; }
-                if(sodien < 50)  { tiendien += (sodien-50)*1734; sodien=50; }
+                if(sodien < 50)  { tiendien += sodien*1678; sodien<50; }
                 tiendien += sodien*1678;
                 printf("So tien dien can dong: %.0f VND\n", tiendien);
                 break;
